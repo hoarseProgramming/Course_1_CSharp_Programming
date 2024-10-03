bbb996c2e1f04dfe1212a6860df5c11d189c3de7
hoarseProgramming <hampus.es@gmail.com>
2024-10-03 13:26:35 +0200

    Add lecture 019 code

---
+++ Lectures/L019_Yield_Return/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+//yield return keeps track of where it was after each time it´s called upon.
+
+foreach (var number in GetNumbers())
+{
+    Console.WriteLine(number);
+}
+
+IEnumerable GetNumbers()
+{
+    yield return 1;
+    yield return 5;
+    yield return 3;
+}
